19fb349a9bb98fcf759a6f04947e575b631d2cc0
ahmed arhab <nowfarahmedarhab@gmail.com>
2025-12-08 01:42:29 +0530

    Edited seeshap

---
+++ seeshap.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Hello, World!");
+        
+        // Interpolation
+        int age = 25;
+        Console.WriteLine($"I am {age} years old.");
+    }
+}
